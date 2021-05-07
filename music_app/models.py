8267977999957e360da58dad1c6d618a8509fe32
FENR1 USER <fenr1user@biadsconsulting.org>
2021-05-07 23:47:29 +0600

    music model change

--- music_app/models.py
+++ music_app/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 # Create your models here.
 class AlbumCategory(models.Model):
-    category = models.CharField(max_length=100)
+    category_name = models.CharField(max_length=100)
     def __str__(self):
-        return self.category
+        return self.category_name
 
 class MusicList(models.Model):
     name = models.CharField(max_length=500)
-    category = models.ForeignKey(AlbumCategory,on_delete=models.CASCADE)
+    category_name = models.ForeignKey(AlbumCategory,on_delete=models.CASCADE)
+    upload_music = models.FileField()
     def __str__(self):
-        return self.category
+        return self.category_name
     
